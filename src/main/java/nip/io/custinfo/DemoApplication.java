909aa9b97312adfd489ccf9325291c24c3d307ff
Administrator <bishnunbl@gmail.com>
2018-09-26 07:46:33 -0400

    Adding Hello

--- src/main/java/nip/io/custinfo/DemoApplication.java
+++ src/main/java/nip/io/custinfo/DemoApplication.java
@@ -5,6 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
+	public String hello() {
+		return "hello how are you??? ";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
